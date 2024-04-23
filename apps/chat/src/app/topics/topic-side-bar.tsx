ac681dc3a5a6a0dd876ccff1cb2f32d9d656b817
Kevin Ferri <kevin@puzzle.io>
2024-04-23 01:40:41 -0400

    stash

--- apps/chat/src/app/topics/topic-side-bar.tsx
+++ apps/chat/src/app/topics/topic-side-bar.tsx
@@ -5,30 +5,40 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function TopicSideBar() {
   return (
-    <div className="flex flex-col shadow-md border-l max-w-[340px] min-w-[340px] basis-full h-full overflow-hidden">
+    <div className="flex flex-col shadow-md border-l max-w-[380px] min-w-[380px] basis-full h-full overflow-hidden">
       <Tabs defaultValue="highlights" className="h-full">
         <div className="p-3">
           <TabsList className="grid w-full grid-cols-3">
             <TabsTrigger value="highlights">
               <StarIcon />
             </TabsTrigger>
+
             <TabsTrigger value="media">
               <ImageIcon />
             </TabsTrigger>
+
             <TabsTrigger value="notifications">
               <BellIcon />
             </TabsTrigger>
           </TabsList>
         </div>
 
-        <ScrollArea className="h-full">
-          <TabsContent value="highlights">
+        <TabsContent value="highlights" className="h-full">
+          <div className="text-center pb-3 shadow-sm">Top highlights</div>
+          <ScrollArea className="h-full">
             <TopHighlights />
-          </TabsContent>
-        </ScrollArea>
+          </ScrollArea>
+        </TabsContent>
 
-        <TabsContent value="media">Media</TabsContent>
-        <TabsContent value="notifications">Notifications</TabsContent>
+        <TabsContent value="media" className="h-full">
+          <div className="text-center pb-3 shadow-sm">Media</div>
+          <ScrollArea className="h-full">Media here</ScrollArea>
+        </TabsContent>
+
+        <TabsContent value="notifications" className="h-full">
+          <div className="text-center pb-3 shadow-sm">Notifications</div>
+          <ScrollArea className="h-full">Notifications here</ScrollArea>
+        </TabsContent>
       </Tabs>
     </div>
   );
