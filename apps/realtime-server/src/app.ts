17628fff18d9c787e33d015abfb21cf348ca6fc3
Kevin Ferri <kevin@puzzle.io>
2024-08-24 13:49:14 -0500

    Health check

--- apps/realtime-server/src/app.ts
+++ apps/realtime-server/src/app.ts
@@ -2,9 +2,16 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { registerEventHandlers } from "./event-handlers/main";
 import { middleware } from "./middleware";
+import { parse } from "url";
 
 const port = process.env.PORT;
-const httpServer = createServer();
+const httpServer = createServer((req, res) => {
+  // Health check TODO: log somewhere?
+  if (req.method === "GET" && parse(req.url, true).pathname === "/api/ping") {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ message: "pong" }));
+  }
+});
 
 const wsServer = new Server(httpServer, {
   path: "/ws/",
