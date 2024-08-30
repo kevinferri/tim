01b79c5c91b597490b5ba04f46438fd4c7fd49b8
Kevin Ferri <kevin@puzzle.io>
2024-08-29 21:08:02 -0400

    Interesting

--- apps/chat/src/components/topics/message-sent-at.tsx
+++ apps/chat/src/components/topics/message-sent-at.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import { formatDate } from "@/lib/utils";
+import { useMemo } from "react";
 
 type Props = {
   sentAt: Date;
 };
 
 export function MessageSentAt(props: Props) {
+  const sentAt = useMemo(() => formatDate(props.sentAt), [props.sentAt]);
+
   return (
     <>
       {" "}
       <time suppressHydrationWarning className="text-slate-300 text-xs">
-        {formatDate(props.sentAt)}
+        {sentAt}
       </time>
     </>
   );
