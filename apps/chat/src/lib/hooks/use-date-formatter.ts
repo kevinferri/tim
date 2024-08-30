a82f9ca82a76ced0ce700edab526301db41b10f4
Kevin Ferri <kevin@puzzle.io>
2024-08-29 22:00:08 -0400

    Fix

--- apps/chat/src/lib/hooks/use-date-formatter.ts
+++ apps/chat/src/lib/hooks/use-date-formatter.ts
@@ -3,11 +3,9 @@
 import { useState } from "react";
 import { useEffectOnce } from "./use-effect-once";
 
-function format(date?: Date) {
-  if (!date) return undefined;
-
+function format(date: Date) {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  return date.toLocaleDateString("en-US", {
+  return new Date(date).toLocaleDateString("en-US", {
     timeZone,
     day: "numeric",
     month: "short",
@@ -21,6 +19,6 @@ function format(date?: Date) {
  */
 export function useDateFormatter(date?: Date) {
   const [d, setD] = useState<string>();
-  useEffectOnce(() => setD(format(date)));
+  useEffectOnce(() => setD(date ? format(date) : undefined));
   return d;
 }
