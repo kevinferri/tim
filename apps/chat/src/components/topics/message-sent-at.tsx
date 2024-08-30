3c6412706398fb0e9605d3ab15fbece8cd7cc4ed
Kevin Ferri <kevin@puzzle.io>
2024-08-29 21:14:06 -0400

    last try

--- apps/chat/src/components/topics/message-sent-at.tsx
+++ apps/chat/src/components/topics/message-sent-at.tsx
@@ -1,20 +1,28 @@
 "use client";
 
-import { formatDate } from "@/lib/utils";
 import { useMemo } from "react";
 
 type Props = {
   sentAt: Date;
 };
 
-export function MessageSentAt(props: Props) {
-  const sentAt = useMemo(() => formatDate(props.sentAt), [props.sentAt]);
+function format(date: Date) {
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  return date.toLocaleDateString("en-US", {
+    timeZone,
+    day: "numeric",
+    month: "short",
+    hour: "numeric",
+    minute: "numeric",
+  });
+}
 
+export function MessageSentAt(props: Props) {
   return (
     <>
       {" "}
       <time suppressHydrationWarning className="text-slate-300 text-xs">
-        {sentAt}
+        {format(props.sentAt)}
       </time>
     </>
   );
