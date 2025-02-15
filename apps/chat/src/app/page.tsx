c913b76aca49c0ec3cbd27e86fe5198d5ed4595c
Kevin Ferri <kevinferri@gmail.com>
2025-02-15 12:14:45 -0500

    test

--- apps/chat/src/app/page.tsx
+++ apps/chat/src/app/page.tsx
@@ -4,56 +4,56 @@ import { redirect } from "next/navigation";
 
 export default async function HomePage() {
   const userId = await getLoggedInUserId();
-  const mostRecentTopic = await prismaClient.topicHistory.findFirst({
-    orderBy: {
-      createdAt: "desc",
-    },
-    where: {
-      userId,
-    },
-    select: {
-      id: true,
-      topicId: true,
-      topic: {
-        select: {
-          circleId: true,
-          parentCircle: {
-            select: {
-              id: true,
-              members: {
-                select: {
-                  id: true,
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  });
+  // const mostRecentTopic = await prismaClient.topicHistory.findFirst({
+  //   orderBy: {
+  //     createdAt: "desc",
+  //   },
+  //   where: {
+  //     userId,
+  //   },
+  //   select: {
+  //     id: true,
+  //     topicId: true,
+  //     topic: {
+  //       select: {
+  //         circleId: true,
+  //         parentCircle: {
+  //           select: {
+  //             id: true,
+  //             members: {
+  //               select: {
+  //                 id: true,
+  //               },
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
 
-  const isStillInCircle = mostRecentTopic?.topic.parentCircle.members.find(
-    ({ id }) => id === userId
-  );
+  // const isStillInCircle = mostRecentTopic?.topic.parentCircle.members.find(
+  //   ({ id }) => id === userId
+  // );
 
-  if (
-    mostRecentTopic?.topicId &&
-    mostRecentTopic?.topic.circleId &&
-    isStillInCircle
-  ) {
-    redirect(
-      `/circles/${mostRecentTopic.topic.circleId}/topics/${mostRecentTopic?.topicId}`
-    );
-  }
+  // if (
+  //   mostRecentTopic?.topicId &&
+  //   mostRecentTopic?.topic.circleId &&
+  //   isStillInCircle
+  // ) {
+  //   redirect(
+  //     `/circles/${mostRecentTopic.topic.circleId}/topics/${mostRecentTopic?.topicId}`
+  //   );
+  // }
 
-  if (mostRecentTopic && !isStillInCircle) {
-    prismaClient.topicHistory.deleteMany({
-      where: {
-        topicId: mostRecentTopic.id,
-        userId,
-      },
-    });
-  }
+  // if (mostRecentTopic && !isStillInCircle) {
+  //   prismaClient.topicHistory.deleteMany({
+  //     where: {
+  //       topicId: mostRecentTopic.id,
+  //       userId,
+  //     },
+  //   });
+  // }
 
   return (
     <div className="flex basis-full justify-center items-center">
