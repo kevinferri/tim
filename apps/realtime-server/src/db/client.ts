c9859c2afc6543e2a2ce3284994a7e9ae1b35f2e
Kevin Ferri <kevin@puzzle.io>
2025-02-20 11:57:01 -0500

    Add cert

--- apps/realtime-server/src/db/client.ts
+++ apps/realtime-server/src/db/client.ts
@@ -9,6 +9,12 @@ const pgClientSingleton = () => {
     client: "pg",
     connection: {
       connectionString: process.env.DATABASE_URL,
+      ssl: {
+        ca: Buffer.from(process.env.DATABASE_SSL_CERT, "base64").toString(
+          "utf-8"
+        ),
+        rejectUnauthorized: true,
+      },
     },
     searchPath: ["knex", "public"],
     useNullAsDefault: true,
