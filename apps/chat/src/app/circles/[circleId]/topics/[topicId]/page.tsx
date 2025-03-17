a9d2b392cea18d60b15f4905c5cac06977139138
Kevin Ferri <kevinferri@gmail.com>
2025-03-17 17:24:48 -0400

    Fix old titles, request only what's needed

--- apps/chat/src/app/circles/[circleId]/topics/[topicId]/page.tsx
+++ apps/chat/src/app/circles/[circleId]/topics/[topicId]/page.tsx
@@ -1,5 +1,3 @@
-import { cache } from "react";
-
 import { prismaClient } from "@/lib/prisma/client";
 import { TopicHeader } from "@/components/topics/topic-header";
 import { TopicChat } from "@/components/topics/topic-chat";
@@ -19,9 +17,31 @@ type Props = {
   params: { topicId: string };
 };
 
-const getTopic = cache(async (id: string) => {
+export async function generateMetadata({ params }: Props) {
   const topic = await prismaClient.topic.getMeTopicById({
-    topicId: id,
+    topicId: params.topicId,
+    select: {
+      id: true,
+      name: true,
+      parentCircle: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  return {
+    title: !topic
+      ? DEFAULT_TITLE
+      : `${topic.parentCircle.name} - ${topic.name}`,
+  };
+}
+
+export default async function TopicPage({ params }: Props) {
+  const topic = await prismaClient.topic.getMeTopicById({
+    topicId: params.topicId,
     select: {
       id: true,
       name: true,
@@ -36,22 +56,6 @@ const getTopic = cache(async (id: string) => {
       },
     },
   });
-
-  return topic;
-});
-
-export async function generateMetadata({ params }: Props) {
-  const topic = await getTopic(params.topicId);
-
-  return {
-    title: !topic
-      ? DEFAULT_TITLE
-      : `${topic.parentCircle.name} - ${topic.name}`,
-  };
-}
-
-export default async function TopicPage({ params }: Props) {
-  const topic = await getTopic(params.topicId);
 
   const queries = [
     prismaClient.message.getMessagesForTopic({
