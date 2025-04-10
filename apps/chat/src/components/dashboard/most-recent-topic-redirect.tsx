f2763e7b215763704517d31568f66e5a373dbff9
Kevin Ferri <kevinferri@gmail.com>
2025-04-09 23:06:10 -0400

    Loader fixes

--- apps/chat/src/components/dashboard/most-recent-topic-redirect.tsx
+++ apps/chat/src/components/dashboard/most-recent-topic-redirect.tsx
@@ -10,13 +10,24 @@ type Props = {
   redirect: string;
 };
 
-function ChatSkeleton() {
+function ChatSkeleton({ index }: { index: number }) {
+  const widths = [
+    "w-[70%]",
+    "w-[60%]",
+    "w-[50%]",
+    "w-[80%]",
+    "w-[65%]",
+    "w-[55%]",
+    "w-[75%]",
+  ];
+  const widthClass = widths[index % widths.length];
+
   return (
-    <div className="flex items-start space-x-4 p-4 w-full">
+    <div className="flex items-start space-x-2 p-3 w-full">
       <Skeleton className="h-10 w-10 rounded-full shrink-0" />
       <div className="flex flex-col space-y-2 w-full">
         <Skeleton className="h-4 w-[120px]" />
-        <Skeleton className="h-4" />
+        <Skeleton className={`h-4 ${widthClass}`} />
       </div>
     </div>
   );
@@ -33,7 +44,7 @@ export function MostRecentTopicRedirect(props: Props) {
     <div className="flex h-screen w-full">
       <div className="shadow-md border-r hidden max-w-[200px] min-w-[220px] lg:max-w-[280px] lg:min-w-[280px] md:flex flex-col">
         {[...Array(LOADER_COUNT)].map((_, i) => (
-          <div className="flex items-start space-x-4 p-4 w-full" key={i}>
+          <div className="flex items-start space-x-3 p-3 w-full" key={i}>
             <div className="flex flex-col space-y-2 w-full">
               <Skeleton className="h-8" />
             </div>
@@ -42,12 +53,12 @@ export function MostRecentTopicRedirect(props: Props) {
       </div>
       <div className="flex-1">
         {[...Array(LOADER_COUNT)].map((_, i) => (
-          <ChatSkeleton key={i} />
+          <ChatSkeleton key={i} index={i} />
         ))}
       </div>
       <div className="shadow-md border-l hidden w-[280px] lg:w-[320px] md:flex flex-col">
         {[...Array(LOADER_COUNT)].map((_, i) => (
-          <ChatSkeleton key={i} />
+          <ChatSkeleton key={i} index={LOADER_COUNT - 1 - i} />
         ))}
       </div>
     </div>
